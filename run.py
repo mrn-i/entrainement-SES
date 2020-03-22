4f4ed2fe9813a72c43fd8b0515755b76a6761e1d
marine <marine.iuso@gmail.com>
2020-03-22 21:57:18 +0100

    change host for prroduction

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from linear_app import app
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+# Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
