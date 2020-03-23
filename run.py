cbafefe2029cae955f2dede2dff09341e3d97421
marine <marine.iuso@gmail.com>
2020-03-23 01:20:42 +0100

    modified prod config

--- run.py
+++ run.py
@@ -5,7 +5,9 @@ import os
 
 if __name__ == "__main__":
 # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
-
+    if os.environ["APP_SETTINGS"] == "config.ProductionConfig":
+        port = int(os.environ.get('PORT', 5000))
+        app.run(debug=True, host='0.0.0.0', port=port)
+    else :
+        app.run(debug=True)
 
