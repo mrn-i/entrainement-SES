c464f0a7c748919ce6c363411e5feac4394ee6f5
marine <marine.iuso@gmail.com>
2020-03-23 01:09:49 +0100

    remove dev config fr host

--- run.py
+++ run.py
@@ -5,10 +5,7 @@ import os
 
 if __name__ == "__main__":
 # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
-    if os.environ["APP_SETTINGS"] == "config.ProductionConfig" :
-        port = int(os.environ.get('PORT', 5000))
-        app.run(debug=True, host='0.0.0.0', port=port)
-    else :
-        app.run(debug=True)
 
