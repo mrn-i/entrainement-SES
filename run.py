edc641588123f945ce7be55a02b8c3879efe036a
marine <marine.iuso@gmail.com>
2020-05-04 21:54:47 +0200

    lance uniquement la partie Cours

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-from linear_app import app
+#from linear_app import app
+from cours import app
 import os
 
 if __name__ == "__main__":
+# in development
+    #app.run(debug=True)
+
+# before exporting
 # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
 
-
+   port = int(os.environ.get('PORT', 5000))
+   app.run(debug=True, host='0.0.0.0', port=port)
