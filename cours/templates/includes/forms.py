e389b8a6cfa3b24f2ac5f8ecf42fb7509bd7afee
marine <marine.iuso@gmail.com>
2020-05-04 21:45:26 +0200

    Version 2 (bêta) : retrait Quizz, ajout cours

---
+++ cours/templates/includes/forms.py
@@ -0,0 +1,43 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DateField, SelectField, FieldList
+from wtforms.validators import DataRequired
+
+grouplist = [("Group1", "Groupe 1"), ("Group2", "Groupe 2")]
+
+class WordForm(FlaskForm):
+    word = StringField('......', [DataRequired()])
+
+class SalaryForms(FlaskForm):
+    yearsofexperience = IntegerField('years', validators=[DataRequired()])
+
+class AnswerForms(FlaskForm):
+    nom = StringField('Nom', [DataRequired()])
+    prenom = StringField('Prénom', [DataRequired()])
+    groupe = SelectField('Groupe', choices = grouplist)
+
+    word1 = StringField('......', [DataRequired()])
+    word2 = StringField('......', [DataRequired()])
+    word3 = StringField('......', [DataRequired()])
+    word4 = StringField('......', [DataRequired()])
+    word5 = StringField('......', [DataRequired()])
+    word6 = StringField('......', [DataRequired()])
+    word7 = StringField('......', [DataRequired()])
+    word8 = StringField('......', [DataRequired()])
+    word9 = StringField('......', [DataRequired()])
+    word10 = StringField('......', [DataRequired()])
+    word11 = StringField('......', [DataRequired()])
+    word12 = StringField('......', [DataRequired()])
+    word13 = StringField('......', [DataRequired()])
+    word14 = StringField('......', [DataRequired()])
+    word15 = StringField('......', [DataRequired()])
+    word16 = StringField('......', [DataRequired()])
+    word17 = StringField('......', [DataRequired()])
+    word18 = StringField('......', [DataRequired()])
+
+    submit = SubmitField('Valider')
+
+class AnswerForms2(FlaskForm):
+    nom = StringField('Nom', [DataRequired()])
+    prenom = StringField('Prénom', [DataRequired()])
+    words = FieldList(StringField('....', [DataRequired()]), min_entries=0, max_entries=20)
+    submit = SubmitField('Submit')
---
+++ cours/views.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, url_for,	 request
+
+import os
+
+
+os.environ["APP_SETTINGS"] = "config.ProductionConfig"
+
+app = Flask(__name__)
+#Permet d'importer toutes les variables de configuration
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.config.from_object("config.DevelopmentConfig")
+# app.config['SQLALCHEMY_ECHO'] = True
+#app.config.from_object(os.environ.get('APP_SETTINGS', DevelopmentConfig))
+
+app.config.from_object(os.environ["APP_SETTINGS"])
+app.secret_key = 'development key' # Flask_WTform CSFR protection
+
+from .templates.includes.forms import AnswerForms, AnswerForms2
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/cours_ch7")
+def ch7():
+    return render_template("Chapitre 7.html")
+
+
+
